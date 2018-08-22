e0c55a3f122972fac289c43916dea290996042a5
Kayla Himmelberger <kaylahimmelberger@Edwards-MacBook-Pro.local>
2018-08-22 11:09:59 -0400

    updated JS with psuedocode. updated html with all parts needed and assigned classes to call within the JS. Updated the CSS so it's mostly done other than making sure it's responsive.

---
+++ Assets/javascript/game.js
@@ -0,0 +1,19 @@
+// Assign random goal score to appear at the start of each game in the HTML between 19-120
+// assign to class: .goal
+
+
+// Assign user's score to start at 0 at the start of each--assign to class: .userScore
+// Have values from the images that have been clicked add to the user's score--assign to class: .userScore
+
+
+// Assign random values to the HtML images with values between 1 - 12
+// assign to classes: .overcast, .lightning, .snow, .tornado
+// Hide these values until the user clicks each image (so they have to guess)
+
+
+// restart the game every time the user wins or loses, maybe with a prompt so it's obvious that it restarted?
+// add each win and loss to the appropriate category on the .scoreboard
+
+
+// Maybe see about removing the figcaption beneath each image and instead have it show on.hover
+
