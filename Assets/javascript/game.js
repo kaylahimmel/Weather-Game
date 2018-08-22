0e8f6678deeb50b324eeb19a22fb6ab2673f57fa
Kayla Himmelberger <kaylahimmelberger@Edwards-MacBook-Pro.local>
2018-08-22 16:37:25 -0400

    started coding JS file but didn't get very far...

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,19 +1,46 @@
+// Assign variables
+var startGame = function() {
+    
+}
+
+
+
 // Assign random goal score to appear at the start of each game in the HTML between 19-120
+var min = 19;
+var max_top = 120;
+var goal = min + (max_top - min) * Math.random() + 1
+console.log(goal)
+
 // assign to class: .goal
+var startGame = function() {
+    // Create our array of underscores
+    for (var i = 0; i < chosenState.length; i++) {
+        puzzle.push("_")
+    }
+    console.log(chosenState.length)
+
+    
 
 
 // Assign user's score to start at 0 at the start of each--assign to class: .userScore
 // Have values from the images that have been clicked add to the user's score--assign to class: .userScore
 
 
+
 // Assign random values to the HtML images with values between 1 - 12
 // assign to classes: .overcast, .lightning, .snow, .tornado
+$(document).ready(function() {
+    $(".overcast").click(function() {
+        $("___").val("___");
+    });
+});
 // Hide these values until the user clicks each image (so they have to guess)
+
 
 
 // restart the game every time the user wins or loses, maybe with a prompt so it's obvious that it restarted?
 // add each win and loss to the appropriate category on the .scoreboard
 
 
-// Maybe see about removing the figcaption beneath each image and instead have it show on.hover
 
+// Maybe see about removing the figcaption beneath each image and instead have it show on.hover
