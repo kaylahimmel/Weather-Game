62fc1d1a8063eb2f08a148113408b0c44662b348
Kayla Himmelberger <kaylahimmelberger@Edwards-MacBook-Pro.local>
2018-08-25 10:32:09 -0400

    updated html and js to remove the errors from last night, but am still having issues assigning the random values to the images and getting them to add together.

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,34 +1,14 @@
 // VARIABLES -----------------------------------------------------------------------------------
 // Assign variables
-var choiceClasses = [".overcast", ".lightning", ".storm", ".tornado"]
+var options = [".overcast", ".lightning", ".storm", ".tornado"]
+var choices = options[i];
+var clickedClasses = [];
 var currentScore = 0;
+
 
 // Assign random goal score to appear at the start of each game in the HTML between 19-120
 var ranGoal = Math.floor(Math.random() * 121) + 19;
-console.log(ranGoal)
-
-
-// Start Round or new game
-var startGame = {
-    // set new goal score
-    $(".goal").textContent(ranGoal);
-    // set userscore to 0
-    $(".score").empty();
-    // assign new values to the images
-    $(ranValue).text(choiceClasses[i]);
-
-};
-
-
-// check to see if they won
-var checkWin = {
-    // update the wins/losses section
-    if (userScore === ranGoal ) {
-            wins++;
-    } else if ( userScore > ranGoal) {
-            losses++;
-    }
-};
+console.log(ranGoal);
 
 
 // Create random values between 1 - 12
@@ -36,17 +16,35 @@ var ranValue = Math.floor(Math.random()) + 1;
     console.log(ranValue);
 
 
-// Assign random values (ranValue) to the HTML images
-for (var i = 0; i < choiceClasses.length; i++) {
-    (choiceClasses).indexOf(ranValue);
+// Start Round or new game
+function startGame() {
+    // set new goal score
+    $(".goal").text(ranGoal);
+    // set userscore to 0
+    $(".score").empty();
+    // Assign random values (ranValue) to the HTML images
+    for (var i = 0; i < options.length; i++) {
+        (ranValue).indexOf(options);
+        (classChoices).push(clickedClasses);
+    };
 };
 
 
-// Have values from the images that have been clicked add to the user's score--assign to HTML class: .userScore
-// something like this code maybe?
-//var numbering = function(number) {
-    //currentNumber = currentNumber.concat(number);
-var classChoices = $(".choices").on("click", function() {
-    var number = parseInt($(this).attr("ranValue"));
-    currentScore += number;
-});
+// Add the clickedClasses values to the user's score--assign to HTML class: .userScore
+function classChoices() {
+    $(".weather").on("click", function() {
+        var number = parseInt($(this).attr("ranValue"));
+        currentScore += number;
+    });
+};
+
+
+// check to see if they won
+function checkWin() {
+    // update the wins/losses section
+    if (userScore === ranGoal ) {
+            wins++;
+    } else if ( userScore > ranGoal) {
+            losses++;
+    }
+};
