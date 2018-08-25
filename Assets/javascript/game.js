699e417765156bc1012eec90b68e61c299872ab6
Kayla Himmelberger <kaylahimmelberger@Edwards-MacBook-Pro.local>
2018-08-24 22:04:53 -0400

    updated js and html. js is erroring out lines 14-26 (and possibly other lines.)

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,70 +1,52 @@
 // VARIABLES -----------------------------------------------------------------------------------
 // Assign variables
 var choiceClasses = [".overcast", ".lightning", ".storm", ".tornado"]
+var currentScore = 0;
 
-$(startGame) = 
+// Assign random goal score to appear at the start of each game in the HTML between 19-120
+var ranGoal = Math.floor(Math.random() * 121) + 19;
+console.log(ranGoal)
+
+
+// Start Round or new game
+var startGame = {
     // set new goal score
-    $(ranGoal) = 0;
+    $(".goal").textContent(ranGoal);
     // set userscore to 0
-    $(".userScore") = 0;
+    $(".score").empty();
     // assign new values to the images
-    $(ranValue).textContent(choiceClasses[i]) {
+    $(ranValue).text(choiceClasses[i]);
+
+};
+
+
+// check to see if they won
+var checkWin = {
     // update the wins/losses section
-        if function() {
-            userScore === ranGoal {
-                wins++;
-            }
-        }
-        else if function() {
-            userScore > ranGoal {
-                losses++;
-            }
-        }
-    };
+    if (userScore === ranGoal ) {
+            wins++;
+    } else if ( userScore > ranGoal) {
+            losses++;
+    }
+};
 
 
 // Create random values between 1 - 12
-var ranValue = function() {
-    var minValue = 1;
-    var maxValue = 12;
-    var ranValue = minValue + (maxValue - minValue) * Math.random() + 1;
-    console.log(ranValue)
-}
-
-// assign "ranValue" to each image using HTML classes: .overcast, .lightning, .snow, .tornado
+var ranValue = Math.floor(Math.random()) + 1;
+    console.log(ranValue);
 
 
-
-
-
-
-// Assign random values to the HTML images
+// Assign random values (ranValue) to the HTML images
 for (var i = 0; i < choiceClasses.length; i++) {
-    (ranValue).indexOf(choiceClasses);
-}
-
-// Assign random goal score to appear at the start of each game in the HTML between 19-120
-var ranGoal = function() {
-var minGoal = 19;
-var maxGoal = 120;
-var ranGoal = minGoal + (maxGoal - minGoal) * Math.random() + 1
-console.log(ranGoal)
-}
+    (choiceClasses).indexOf(ranValue);
+};
 
 
 // Have values from the images that have been clicked add to the user's score--assign to HTML class: .userScore
 // something like this code maybe?
 //var numbering = function(number) {
     //currentNumber = currentNumber.concat(number);
-
-
-// Hide these values until the user clicks each image (so they have to guess)
-
-
-
-// restart the game every time the user wins or loses, maybe with a prompt so it's obvious that it restarted?
-// add each win and loss to the appropriate category on the .scoreboard
-
-
-
-// Maybe see about removing the figcaption beneath each image and instead have it show on.hover
+var classChoices = $(".choices").on("click", function() {
+    var number = parseInt($(this).attr("ranValue"));
+    currentScore += number;
+});
